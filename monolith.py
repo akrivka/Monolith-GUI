ad09ecb3c5b075a872ff73b1b8c5bb15036c9e14
Adam Krivka <akrivka@caltech.edu>
2024-03-03 14:35:32 -0800

    remove temporary code and file

--- monolith.py
+++ monolith.py
@@ -5,24 +5,10 @@
 """
 
 from canbus.canbus import Canbus
-from canbus.canbus_ni import read
-
 
 
 def main():
-    # Create the Canbus interface
-    canbus = Canbus("canbus/interfaces.toml")
-
-    # Loop to receive messages
-    while True:
-        # Receive a message
-        message = canbus.recv()
-
-        # Print the message
-        print(message)
-
-        # Send the message back
-        canbus.send(message)
+    
 
 
 if __name__ == "__main__":
